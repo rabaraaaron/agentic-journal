ed9b58ab99987ba13bfc96a4f2d9802ff7da19a4
Aaron Rabara <rabara_aaron@bah.com>
2025-08-25 13:45:49 -0400

    Small changes

--- server/src/service/llm_service.py
+++ server/src/service/llm_service.py
@@ -6,8 +6,8 @@ class LLMService:
 
     def __init__(self):
         self.llm = ChatOllama(
-            model="qwen2.5:7b",
-            base_url=os.getenv("OLLAMA_URL"),
+            model=os.getenv("LLM_MODEL", None),
+            base_url=os.getenv("OLLAMA_URL", None),
             temperature=0.7,
             num_predict=512,      # Limit output length for speed
             keep_alive="10m",
